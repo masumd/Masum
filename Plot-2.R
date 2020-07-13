18a97a08bba3b9e0de05599562aac29bd1db46d2
masumd <46015154+masumd@users.noreply.github.com>
2020-07-13 09:38:14 +0530

    Project-2 Plot-2

---
+++ Plot-2.R
@@ -0,0 +1,11 @@
+if(!exists("NEI")){
+  NEI <- readRDS("C:/Users/masum/Desktop/Data Analysis Course/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("C:/Users/masum/Desktop/Data Analysis Course/Source_Classification_Code.rds")
+}
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
